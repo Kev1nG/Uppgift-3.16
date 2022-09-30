f66a09832e3656ed8c8dd815679e5f1fd1b6ecc4
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-30 14:37:36 +0200

    Add project files.

---
+++ Uppgift 3.16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_3._16
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur lång låt är den låten du vill spela? (min 2m 45sek och max 4m 20sek)");
+            int längdm = int.Parse(Console.ReadLine());
+            int sek = int.Parse(Console.ReadLine());
+            int m = längdm * 60;
+            int tot = m + sek;
+
+            if (tot <= 165 || tot >= 265) 
+            {
+                Console.WriteLine("Denna låt får inte spelas på radiostaionen");
+            }
+            else
+            {
+                Console.WriteLine("Denna låt får du tyvärr inte spelas");
+            }
+        }
+    }
+}
